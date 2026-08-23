eb14b556a98aed80b78bc70bcf2a39a1bc080e42
xrip <xrip@xrip.ru>
2026-08-23 10:15:14 +0300

    test(graphics): lock legacy display defaults

--- test/DETHRACE/test_graphics.c
+++ test/DETHRACE/test_graphics.c
@@ -1,6 +1,22 @@
 #include "tests.h"
 
+#include "common/car.h"
 #include "common/graphics.h"
+#include "dr_types.h"
+
+extern tGraf_spec gGraf_specs[2];
+
+void test_graphics_defaults() {
+    TEST_ASSERT_EQUAL_INT(320, gGraf_specs[0].total_width);
+    TEST_ASSERT_EQUAL_INT(200, gGraf_specs[0].total_height);
+    TEST_ASSERT_EQUAL_INT(640, gGraf_specs[1].total_width);
+    TEST_ASSERT_EQUAL_INT(480, gGraf_specs[1].total_height);
+
+    TEST_ASSERT_EQUAL_INT(0, gCar_simplification_level);
+    TEST_ASSERT_FLOAT_WITHIN(0.001f, 10.0f, gCar_simplification_factor[0][0]);
+    TEST_ASSERT_FLOAT_WITHIN(0.001f, 3.0f, gCar_simplification_factor[0][1]);
+    TEST_ASSERT_FLOAT_WITHIN(0.001f, 5.0f, gCar_simplification_factor[1][1]);
+}
 
 void test_graphics_loadfont() {
     REQUIRES_DATA_DIRECTORY();
@@ -13,5 +29,6 @@ void test_graphics_loadfont() {
 
 void test_graphics_suite() {
     UnitySetTestFile(__FILE__);
+    RUN_TEST(test_graphics_defaults);
     RUN_TEST(test_graphics_loadfont);
 }
